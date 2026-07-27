8bd0cd26e102c10fba30257a45204e2863fd4ddb
Oluwakayode <ikayode007@gmail.com>
2026-07-27 23:20:06 +0100

    Added raylib to the project.

--- src/pong.m.cpp
+++ src/pong.m.cpp
@@ -1,7 +1,25 @@
-#include <iostream>
+#include <raylib.h>
 
 int main(int argc, char *argv[]) {
     (void)argc;
     (void)argv;
-    std::cout << "Welcome to my setup" << std::endl;
+
+    constexpr int screenWidth = 1280;
+    constexpr int screenHeight = 720;
+
+    InitWindow(screenWidth, screenHeight, "My raylib game");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        ClearBackground(BLACK);
+        DrawText("raylib was built with the game", 40, 40, 30, RAYWHITE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
 }
